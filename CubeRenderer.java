e9fe50aeeacfe0d4696a1a5f56e6983a6009c9f0
lion4202 <brickbuilder2024@gmail.com>
2022-10-19 12:48:33 -0400

    First commit! (I think; I'm new to this github intergration stuff)

---
+++ CubeRenderer.java
@@ -0,0 +1,18 @@
+import processing.core.*;
+
+public class CubeRenderer extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("CubeRenderer");
+    }
+
+    public void settings() {
+        size(1000, 1000, P3D);
+    }
+    public void setup() {
+        background(37, 193, 232);
+        noStroke();
+    }
+    public void draw() {
+
+    }
+}
---
+++ Piece.java
@@ -0,0 +1,11 @@
+public class Piece {
+    //variables, including PShape (I think)
+
+    //constructor
+
+    //build center method
+
+    //build edge method
+
+    //build corner method
+}
